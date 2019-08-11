2506b0f6ec544222d66b94044635e03da597b6fa
BaHaMuT7 <raul.pardoz@gmail.com>
2019-08-10 20:37:39 -0400

    Creación de proyecto base - Se crea una plantalla para trabajar con bootstrap 4 - Se crean paquetes para trabajo con MVC - Se crea una clase main para pruebas - Se crea controlador-plantilla con cotejamiento UTF-8 en petición y respuesta

---
+++ src/java/cl/segurosporsiempre/Connection/Ejecutar.java
@@ -0,0 +1,20 @@
+/*
+ * Portafolio de título
+ * Seguros por siempre
+ */
+package cl.segurosporsiempre.Connection;
+
+/**
+ *
+ * @author Raúl Pardo Zurita
+ */
+public class Ejecutar {
+
+    /**
+     * @param args los argumentos de la línea de comandos
+     */
+    public static void main(String[] args) {
+        // TODO Ejecutar pruebas acá
+    }
+    
+}
---
+++ src/java/cl/segurosporsiempre/Controller/LoginController.java
@@ -0,0 +1,52 @@
+/*
+ * Portafolio de título
+ * Seguros por siempre
+ */
+package cl.segurosporsiempre.Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Raúl Pardo Zurita
+ */
+public class LoginController extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request petición
+     * @param response respuesta
+     * @throws ServletException si ocurre un error en el controlador
+     * @throws IOException si ocurre un error de entrada y salida
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request petición
+     * @param response respuesta
+     * @throws ServletException si ocurre un error en el controlador
+     * @throws IOException si ocurre un error de entrada y salida
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        
+    }
+}
